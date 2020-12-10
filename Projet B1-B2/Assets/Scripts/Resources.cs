7c461fa47e09fd48d5440411894227bf3c6dd37b
Fehrenbach <baptiste@steloria.fr>
2020-12-10 19:03:53 +0100

    Start Resources script

--- Projet B1-B2/Assets/Scripts/Resources.cs
+++ Projet B1-B2/Assets/Scripts/Resources.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class Resources : MonoBehaviour
 {
-    int qte = 0;
+    public string name = "";
+    public int level = 1;
+
+    int qteOnClick = 0;
+    int upgradePrice = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (name)
+        {
+            case "Fer":
+                qteOnClick = 100;
+                upgradePrice = 20;
+                break;
+            case "Nickel":
+                qteOnClick = 75;
+                upgradePrice = 30;
+                break;
+            case "Silicate":
+                qteOnClick = 60;
+                upgradePrice = 40;
+                break;
+            case "Silicium":
+                qteOnClick = 45;
+                upgradePrice = 70;
+                break;
+            case "Méthane":
+                qteOnClick = 30;
+                upgradePrice = 100;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject go = GameObject.Find("ResourceManager");
+            ResourcesManager other = (ResourcesManager) go.GetComponent(typeof(ResourcesManager));
+            other.addResources(name, qteOnClick);
+        }
     }
 
     // Update is called once per frame
---
+++ Projet B1-B2/Assets/Scripts/ResourcesManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesManager : MonoBehaviour
+{
+    int iron = 0;
+    int nickel = 0;
+    int silicate = 0;
+    int silicium = 0;
+    int methane = 0;
+
+    public Text ironText;
+    public Text nickelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ironText.text = "Fer: " + iron.ToString();
+        nickelText.text = "Nickel: " + nickel.ToString();
+    }
+
+    public void addResources(string name, int qte) {
+        switch (name)
+        {
+            case "Fer":
+                iron += qte;
+                break;
+            case "Nickel":
+                nickel += qte;
+                break;
+            case "Silicate":
+                silicate += qte;
+                break;
+            case "Silicium":
+                silicium += qte;
+                break;
+            case "Méthane":
+                methane += qte;
+                break;
+            default:
+                break;
+        }
+    }
+}
