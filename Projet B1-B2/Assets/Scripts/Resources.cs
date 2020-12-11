bef1414161b7caefdff1f8c2eff7bbc32e084e27
Fehrenbach <baptiste@steloria.fr>
2020-12-11 18:47:43 +0100

    Rework Resources

--- Projet B1-B2/Assets/Scripts/Resources.cs
+++ Projet B1-B2/Assets/Scripts/Resources.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Resources : MonoBehaviour
 {
-    public string name = "";
-    public int level = 1;
+    // Variables modifiables depuis l'inspecteur
+    public string name = ""; // Nom de la ressource
+    public int level = 1; // Niveau du bâtiment (inutile pour le moment)
 
-    int qteOnClick = 0;
-    int upgradePrice = 0;
+    // Variables qui dépendent du type de la ressource
+    int qteOnClick = 0; // Quantité de ressources récupérables au click sur le bâtiment
+    int upgradePrice = 0; // Prix de l'augmentation de niveau (inutile pour le moment)
+    int qtePerTime = 15; // Quantité de ressources produites tous les x temps
+    int timeToGetResource = 5; // Temps nécessaire à la production de ressources
+
+    // Variables propre au script
+    int stock = 0; // Stock actuel du bâtiment
+    float timer = 0; // Temps écoulé actuellement
+
+    TextMesh text; // Texte affiché au dessus du bâtiment
+    ResourcesManager rm; // Ressource manager (le canvas)
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject child = gameObject.transform.GetChild(0).gameObject;
+        text = (TextMesh)child.GetComponent(typeof(TextMesh)); // On récupère le GameObject du texte
+
+        GameObject go = GameObject.Find("ResourceManager");
+        rm  = (ResourcesManager) go.GetComponent(typeof(ResourcesManager)); // On récupère le Canvas
+
+        // On récupère le nom de la ressources pour mettre les bonnes valeurs de variables
         switch (name)
         {
             case "Fer":
@@ -22,6 +40,8 @@ public class Resources : MonoBehaviour
             case "Nickel":
                 qteOnClick = 75;
                 upgradePrice = 30;
+                qtePerTime = 10;
+                timeToGetResource = 7;
                 break;
             case "Silicate":
                 qteOnClick = 60;
@@ -40,17 +60,30 @@ public class Resources : MonoBehaviour
         }
     }
 
+    // Si on clique sur le bâtiment, on éxécute ce bloc
     void OnMouseOver() {
         if (Input.GetMouseButtonDown(0)) {
-            GameObject go = GameObject.Find("ResourceManager");
-            ResourcesManager other = (ResourcesManager) go.GetComponent(typeof(ResourcesManager));
-            other.addResources(name, qteOnClick);
+            /* 
+                1) vérifier qu'on ait suffisamment de ressources pour en récupérer
+                2) récupérer lesdites ressources
+                3) supprimer les ressources du stock
+            */
+            rm.addResources(name, qteOnClick);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        // Si on a atteint le temps limite, on ajoute la bonne quantité de ressource au stock
+        if (timer >= timeToGetResource) {
+            timer -= timeToGetResource;
+            stock += qtePerTime;
+        }
+
+        // Et on l'affiche
+        text.text = name + " " + stock.ToString();
     }
 }
