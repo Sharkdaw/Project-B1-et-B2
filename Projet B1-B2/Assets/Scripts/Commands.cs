801b0cc03f121954420c7a23f1358ccf68f600dd
Rafael <sauzetr@gmail.com>
2021-01-28 18:21:16 +0100

    Change commands script

--- Projet B1-B2/Assets/Scripts/Commands.cs
+++ Projet B1-B2/Assets/Scripts/Commands.cs
@@ -8,6 +8,7 @@ public class Commands : MonoBehaviour
     public Text commandText;
     public Button acceptButton;
     public Button denyButton;
+    public GameObject commandCanvas;
 
     string[] missions = {
         "Bonjour contrebandier. Vous savez à quel point il est difficile de se procurez du fer de qualité par les temps qui cours. L'arrivé de casque Corindon rend certaine transaction... comment dire... officieuse compliqué. (300 Fer)",
@@ -25,17 +26,33 @@ public class Commands : MonoBehaviour
     void Start()
     {
         // Créer un nombre aléatoire et le stocker dans "nextMission"
+        System.Random rnd = new System.Random();
+        nextMission = rnd.Next(15, 30);
+
+        commandCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        
 
-        // Regarder si le temps avant prochaine mission est atteint
-        // Choisir une mission aléatoire
-        // Afficher la mission si les ressources sont suffisantes
-        // Reset le timer
-        // Créer un nouveau nombre aléatoire pour "nextMission"
+        if (timer >= nextMission)
+        {
+            System.Random rnd = new System.Random();
+            int miss  = rnd.Next(2);
+            commandText.text = missions[miss];
+            timer = 0; 
+            nextMission = rnd.Next(15, 30);
+            commandCanvas.SetActive(true);
+            
+        }
+        
+    }
+
+    public void removeCanvas() {
+        
+        commandCanvas.SetActive(false);
     }
 }
